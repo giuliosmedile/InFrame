a4f3553d86bbe762b8c32e3a07b1e89164a3e28e
Giulio Smedile <giu.smedile1@stud.uniroma3.it>
2021-12-22 12:58:26 +0100

    Frame home setup

--- app/src/main/java/com/smeds/inframe/FrameHomeActivity.kt
+++ app/src/main/java/com/smeds/inframe/FrameHomeActivity.kt
@@ -2,10 +2,25 @@ package com.smeds.inframe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.ImageView
+import android.widget.Toast
 
 class FrameHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_home)
+
+        // Set tap listener to image
+        val imageView = findViewById<ImageView>(R.id.frameHomeImageView)
+        imageView.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_UP -> {
+                    Toast.makeText(this, "Image tapped", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            true
+        }
     }
 }
